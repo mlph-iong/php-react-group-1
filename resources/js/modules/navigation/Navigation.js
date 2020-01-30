7cefc71ce2ceb94563758d4e3c33433b4f5ed0cc
Ireene Ong <ireene@ideyatech.com>
2020-01-30 13:52:06 +0800

    Housekeeping: uncomment web php code for home page, change brand icon link

--- resources/js/modules/navigation/Navigation.js
+++ resources/js/modules/navigation/Navigation.js
@@ -9,7 +9,7 @@ const NavBar = () => {
     return (
     <Navbar bg="dark" variant="dark" expand="lg">
         <div className="container">
-            <Navbar.Brand href="#home"><FontAwesomeIcon icon='calendar'></FontAwesomeIcon></Navbar.Brand>
+            <Navbar.Brand><Link to='/'><FontAwesomeIcon icon='calendar'></FontAwesomeIcon></Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
