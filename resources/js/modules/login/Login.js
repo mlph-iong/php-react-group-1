b1949ea729ad23981019b12418fc044bda2197b0
clgarillo <cgarillo@ideyatech.com>
2020-02-13 00:23:33 +0800

    Happy Linter No 1 Little housekeeping to make js linter happy.

--- resources/js/modules/login/Login.js
+++ resources/js/modules/login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Login.css";
+import InputHelper from "../inputHelper/InputHelper";
 import InputValidationMessage from "../validationHelper/validationHelper";
 import axios from 'axios';
 
@@ -72,38 +73,37 @@ class Login extends Component {
         })
 }
 
-  render() {
-      return (
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-6 mt-5 mx-auto">
-                      <form noValidate onSubmit={this.onSubmit}>
-                          <h1 className="h3 mb-3 font-weight-normal">
-                              Please sign in
-                          </h1>
-                          <InputValidationMessage message={this.state.errors["message"]} />
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mt-5 mx-auto">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h1 className="h3 mb-3 font-weight-normal">
+                                Please sign in
+                            </h1>
+                            <InputValidationMessage message={this.state.errors["message"]} />
 
-                          <div className="form-group">
-                              <label htmlFor="username">Username</label>
-                              <input type="username" className="form-control"
-                                  name="username" placeholder="Username"
-                                  value={this.state.username} onChange={this.onChange} />
-                          </div>
-                          <div className="form-group">
-                              <label htmlFor="password">Password</label>
-                              <input type="password" className="form-control"
-                                  name="password" placeholder="Password"
-                                  value={this.state.password} onChange={this.onChange} />
-                          </div>
-                          <button type="submit"
-                              className="btn btn-lg btn-primary btn-block" >
-                              Sign in
-                          </button>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      )
-  }
+                            <InputHelper 
+                                label="Username" name="username" type="text"
+                                className="" placeholder="Username"
+                                value={ this.state.username } onChange={ this.onChange }
+                                errorMessage="" />
+                            <InputHelper 
+                                label="Password" name="password" type="password"
+                                className="" placeholder="Password"
+                                value={ this.state.password } onChange={ this.onChange }
+                                errorMessage="" />
+                            
+                            <button type="submit"
+                                className="btn btn-lg btn-primary btn-block" >
+                                Sign in
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 export default Login
